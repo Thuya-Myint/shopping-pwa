e408b897de8f92b33c888cead7b12bf48a2dcaac
Min-Thant794 <minthanthtun29@gmail.com>
2025-07-17 21:41:49 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Home from './home'
+import Login from './Login'
 const App = () => {
   return (
     <div>
@@ -7,7 +8,8 @@ const App = () => {
        <h1>Sai Saing Wan</h1>
 
       <h1>thuyamyint</h1>
-
+      <Home/>
+      <Login/>
     </div>
   )
 }
